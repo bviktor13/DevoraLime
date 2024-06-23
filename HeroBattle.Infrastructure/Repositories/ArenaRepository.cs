654412776c14e906de90747dd1c557a05e39be0b
Blasszauer Viktor <blasszauer.viktor@nexius.hu>
2024-06-23 16:10:26 +0200

    Add errorhandling and logging

--- HeroBattle.Infrastructure/Repositories/ArenaRepository.cs
+++ HeroBattle.Infrastructure/Repositories/ArenaRepository.cs
@@ -1,44 +1,70 @@
 ﻿using HeroBattle.Domain.Models;
 using HeroBattle.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace HeroBattle.Infrastructure.Repositories
 {
     public class ArenaRepository : IArenaRepository
     {
         private readonly HeroBattleDbContext _context;
+        private readonly ILogger<ArenaRepository> _logger;
 
-        public ArenaRepository(HeroBattleDbContext context)
+        public ArenaRepository(HeroBattleDbContext context, ILogger<ArenaRepository> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<Arena> AddAsync(int numberOfHeroes)
         {
-            var arena = new Arena()
+            try
             {
-                Id = Guid.NewGuid().ToString(),
-                IsFinished = false,
-                NumberOfHeroes = numberOfHeroes
-            };
+                var arena = new Arena()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    IsFinished = false,
+                    NumberOfHeroes = numberOfHeroes
+                };
 
-            _context.Arenas.Add(arena);
-            await _context.SaveChangesAsync();
+                _context.Arenas.Add(arena);
+                await _context.SaveChangesAsync();
 
-            return arena;
+                return arena;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while adding a new arena.");
+                throw;
+            }
         }
 
-        public async Task<Arena?> GetByIdAsync(string id) => await _context.Arenas.Include(x => x.History).SingleOrDefaultAsync(x => x.Id == id);
+        public async Task<Arena?> GetByIdAsync(string id)
+        {
+            try
+            {
+                return await _context.Arenas
+                    .Include(x => x.History)
+                    .SingleOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while getting the arena with Id: {id}.");
+                throw;
+            }
+        }
 
         public async Task UpdateAsync(Arena arena)
         {
-            var arenaToUpdate = await _context.Arenas.SingleOrDefaultAsync(x => x.Id == arena.Id);
-            if (arenaToUpdate is not null)
+            try
             {
-                arenaToUpdate.History = arena.History;
-                arenaToUpdate.IsFinished = true;
-
+                _context.Arenas.Update(arena);
                 await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while updating the arena with Id {arena.Id}.");
+                throw;
             }
         }
     }
